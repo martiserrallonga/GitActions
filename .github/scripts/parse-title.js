85b1a96a42e1535c5b5accc6f7d5a5a56d654983
Martí Serrallonga <martiserrallonga@gmail.com>
2026-03-16 10:57:21 +0100

    [12345][12350] Multiple links (#14)

--- .github/scripts/parse-title.js
+++ .github/scripts/parse-title.js
@@ -1,30 +1,38 @@
 module.exports = {
   events: ["opened", "edited"],
-
   run: async ({ github, context }) => {
     const pr = context.payload.pull_request;
 
     if (context.payload.action === "edited" &&
-      !context.payload.changes?.title)
+        !context.payload.changes?.title)
+      return;
+
+    const matches = [...pr.title.matchAll(/\[(\d+)\]/g)];
+    if (matches.length === 0)
       return;
 
     let body = pr.body || "";
+    let linksToAdd = [];
+    for (const match of matches) {
+      const issueId = match[1];
+      const link = `https://redmine.asuni.net/issues/${issueId}`;
 
-    const match = pr.title.match(/\[(\d+)\]/);
-    if (!match)
+      if (body.includes(link))
+        continue;
+
+      linksToAdd.push(`[Redmine #${issueId}](${link})`);
+    }
+
+    if (linksToAdd.length === 0)
       return;
 
-    const issueId = match[1];
-    const link = `https://redmine.asuni.net/issues/${issueId}`;
-
-    if (!body.includes(link))
-      body = `${body}\n\n[Redmine #${issueId}](${link})`;
+    body = `${body}\n\n${linksToAdd.join("\n")}`;
 
     await github.rest.pulls.update({
       owner: context.repo.owner,
       repo: context.repo.repo,
       pull_number: pr.number,
-      body: body
+      body
     });
   }
 };

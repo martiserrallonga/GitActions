a17229945c72e414edebe91cfccbbe6d9e78cd5a
Martí Serrallonga <marti@asuni.com>
2026-03-26 14:58:41 +0100

    [12345][12350] Redmine API key

--- .github/scripts/parse-title.js
+++ .github/scripts/parse-title.js
@@ -1,3 +1,29 @@
+const fetch = require("node-fetch");
+
+async function getRedmineIssue(issueId, apiKey) {
+  const url = `https://redmine.asuni.net/issues/${issueId}.json`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "X-Redmine-API-Key": apiKey
+      }
+    });
+
+    if (!response.ok) {
+      console.warn(`Error fetching issue ${issueId}`);
+      return null;
+    }
+
+    const data = await response.json();
+    return data.issue;
+
+  } catch (err) {
+    console.error(`Failed to fetch issue ${issueId}`, err);
+    return null;
+  }
+}
+
 module.exports = {
   events: ["opened", "edited"],
   run: async ({ github, context }) => {
@@ -13,14 +39,25 @@ module.exports = {
 
     let body = pr.body || "";
     let linksToAdd = [];
+
+    console.log("API KEY exists:", !!process.env.REDMINE_API_KEY);
+
     for (const match of matches) {
       const issueId = match[1];
+      const issue = await getRedmineIssue(issueId, process.env.REDMINE_API_KEY);
+
+      if (!issue)
+        continue;
+
       const link = `https://redmine.asuni.net/issues/${issueId}`;
+      const title = issue.subject;
+
+      const markdown = `[${title}](${link})`;
 
       if (body.includes(link))
         continue;
 
-      linksToAdd.push(`[Redmine #${issueId}](${link})`);
+      linksToAdd.push(markdown);
     }
 
     if (linksToAdd.length === 0)
